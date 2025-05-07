083fbb402a4348c63ed9e64bc4df8b70cce55d1f
Khadiza Samiha <khadizasamiha7@gmail.com>
2025-05-07 16:51:38 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline text-blue-500 italic">
+       Custom React Hooks!
+     </h1>
+    </>
+  )
+}
+
+export default App
